60b6fa58ab657476c9d6f77c10ca3a574a2f0f33
mohamedatef98 <eng.mohamedatef98@gmail.com>
2018-11-09 18:32:49 +0200

    Assignment #1 dist

---
+++ Assignment #1/Dist/README.md
@@ -0,0 +1,7 @@
+# SIS-SW Assignment #1 Build
+
+This is a built version of the assignment.
+
+Just download (or clone) the repo and serve it using any local server.
+
+A live version is available https://sis-sw.herokuapp.com/
---
+++ Assignment #1/Dist/app/Http/Controllers/CourseController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\Course;
+use Illuminate\Support\Facades\DB;
+
+
+class CourseController extends Controller
+{
+    public function index(){
+        return Course::get();
+    }
+
+    public function show(Course $course){
+        $data = [
+            'id' => $course->id,
+            'name' => $course->name,
+            'max_grade' => $course->max_grade,
+            'students' => []
+        ];
+
+        $students = $course->students;
+        foreach($students as $student){
+            $data['students'][] = [
+                'id' => $student->id,
+                'name' => $student->name,
+                'grade' => $student->grades->where('course_id', $course->id)->first()->grade
+            ];
+        }
+        return $data;
+    }
+
+    public function create(Request $request){
+        Course::create($request->only('name', 'max_grade'));
+
+        return response('{"added": true}', 220);
+    }
+
+    public function destroy(Course $course){
+        \App\Grade::where('course_id', $course->id)->delete();
+
+        DB::table('course_student')->where('course_id', $course->id);
+
+        $course->delete();
+
+        return response('{"deleted":true}', 221);
+    }
+
+    public function search(Request $request){
+        $name = $request->input('q');
+
+        return \App\Course::where('name','like','%'.$name.'%')
+        ->orderBy('name')->get();
+    }
+}
